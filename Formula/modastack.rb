3ba804628399f50b2da1ce482720a0acc056b3ba
Zachary Kozick <zach@modalabs.ai>
2026-06-04 14:43:24 -0700

    fix: use canonical PyPI URL from JSON API

--- Formula/modastack.rb
+++ Formula/modastack.rb
@@ -3,7 +3,7 @@ class Modastack < Formula
 
   desc "AI engineering team: manager + engineer agents with Linear, GitHub, Slack"
   homepage "https://github.com/moda-labs/modastack"
-  url "https://files.pythonhosted.org/packages/source/m/modastack/modastack-0.5.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/cc/2b/05cba392701571bdd8baa91e74703a027383285f9616a265d99bbb03ba39/modastack-0.5.0.tar.gz"
   sha256 "f709734131732d4d8f675fa5275bbeadd636a59e2b26efba530a9f171b380af5"
   license "MIT"
 
