42adbb24a1b7a0d5f5436f8b44c5c0564f206f55
Zachary Kozick <zach@modalabs.ai>
2026-06-04 13:01:15 -0700

    Initial Homebrew tap for modastack

---
+++ Formula/modastack.rb
@@ -0,0 +1,19 @@
+class Modastack < Formula
+  include Language::Python::Virtualenv
+
+  desc "AI engineering team: manager + engineer agents with Linear, GitHub, Slack"
+  homepage "https://github.com/moda-labs/modastack"
+  url "https://files.pythonhosted.org/packages/source/m/modastack/modastack-0.2.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/modastack --help")
+  end
+end
